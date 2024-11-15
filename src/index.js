a3c0c6dac0b6cc1346009b9f0de323098662ade2
jonah <jonahkf@gmail.com>
2024-11-15 10:35:46 +0900

    ADDED: Dummy Divs for buttons & slide indicators

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+const frameController = () => {
+  const frame = document.querySelector(".frame");
+  const imgReelDiv = document.querySelector(".wide");
+
+  // Change margin-left on imgReelDiv on button clicks (negative scrolls right)
+};
+
+frameController();
