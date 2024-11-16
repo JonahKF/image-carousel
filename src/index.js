a7e6069aae32ea03a323e1a320881db12c4d0486
jonah <jonahkf@gmail.com>
2024-11-16 11:51:16 +0900

    ADDED: Ability to scroll right, placeholder indicators

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import "./styles.css";
 const frameController = () => {
   const frame = document.querySelector(".frame");
   const imgReelDiv = document.querySelector(".wide");
+  let currentMarginLeft = 0;
+
+  const leftBtn = document.querySelector(".left-btn");
+  const rightBtn = document.querySelector(".right-btn");
 
   // Change margin-left on imgReelDiv on button clicks (negative scrolls right)
+  leftBtn.addEventListener("click", () => {
+    console.log("Left button pressed");
+  });
+
+  rightBtn.addEventListener("click", () => {
+    currentMarginLeft = currentMarginLeft - 400;
+    imgReelDiv.style.marginLeft = currentMarginLeft + "px";
+    console.log(currentMarginLeft);
+  });
 };
 
 frameController();
