15c56522ede7f000ce78bdee717f17b3ee2f2bb0
jonah <jonahkf@gmail.com>
2024-11-16 15:28:01 +0900

    ADDED: Indicator dots change according to img

--- src/index.js
+++ src/index.js
@@ -11,16 +11,18 @@ const frameController = () => {
   const leftBtn = document.querySelector(".left-btn");
   const rightBtn = document.querySelector(".right-btn");
 
+  const indicators = document.querySelectorAll(".indicator");
+
   // Change margin-left on imgReelDiv on button clicks (negative scrolls right)
   leftBtn.addEventListener("click", () => {
     if (currentMarginLeft * -1 > 0) {
       currentMarginLeft = currentMarginLeft + 400;
       imgReelDiv.style.marginLeft = currentMarginLeft + "px";
-      console.log(currentMarginLeft);
+      setActiveIndicator();
     } else if (currentMarginLeft === 0) {
       currentMarginLeft = (imgCount - 1) * 400 * -1;
       imgReelDiv.style.marginLeft = currentMarginLeft + "px";
-      console.log(currentMarginLeft);
+      setActiveIndicator();
     }
   });
 
@@ -28,16 +30,31 @@ const frameController = () => {
     if (currentMarginLeft >= (imgCount - 2) * 400 * -1) {
       currentMarginLeft = currentMarginLeft - 400;
       imgReelDiv.style.marginLeft = currentMarginLeft + "px";
-      console.log(currentMarginLeft);
+      setActiveIndicator();
     } else if (currentMarginLeft === (imgCount - 1) * 400 * -1) {
       currentMarginLeft = 0;
       imgReelDiv.style.marginLeft = currentMarginLeft + "px";
-      console.log(currentMarginLeft);
+      setActiveIndicator();
     } else {
       console.log(`Current margin is: ${currentMarginLeft}`);
       console.log(`Testing against: ${(imgCount - 2) * 400 * -1}`);
     }
   });
+
+  indicators.forEach((indicator, index) => {
+    indicator.id = index;
+  });
+
+  const setActiveIndicator = () => {
+    const currentImg = (currentMarginLeft / 400) * -1;
+    indicators.forEach((indicator) => {
+      parseInt(indicator.id) === currentImg
+        ? indicator.classList.add("active")
+        : indicator.classList.remove("active");
+    });
+  };
+
+  setActiveIndicator();
 };
 
 frameController();
