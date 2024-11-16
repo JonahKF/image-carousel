4442cd25ee40af6d24be2d6b03ce8a5d1036f4e4
jonah <jonahkf@gmail.com>
2024-11-16 12:02:59 +0900

    ADDED: Right button now loops reel on end

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const frameController = () => {
   const imgReelDiv = document.querySelector(".wide");
   let currentMarginLeft = 0;
 
+  const imgs = imgReelDiv.querySelectorAll("img");
+  const imgCount = imgs.length;
+
   const leftBtn = document.querySelector(".left-btn");
   const rightBtn = document.querySelector(".right-btn");
 
@@ -14,9 +17,18 @@ const frameController = () => {
   });
 
   rightBtn.addEventListener("click", () => {
-    currentMarginLeft = currentMarginLeft - 400;
-    imgReelDiv.style.marginLeft = currentMarginLeft + "px";
-    console.log(currentMarginLeft);
+    if (currentMarginLeft >= (imgCount - 2) * 400 * -1) {
+      currentMarginLeft = currentMarginLeft - 400;
+      imgReelDiv.style.marginLeft = currentMarginLeft + "px";
+      console.log(currentMarginLeft);
+    } else if (currentMarginLeft === (imgCount - 1) * 400 * -1) {
+      currentMarginLeft = 0;
+      imgReelDiv.style.marginLeft = currentMarginLeft + "px";
+      console.log(currentMarginLeft);
+    } else {
+      console.log(`Current margin is: ${currentMarginLeft}`);
+      console.log(`Testing against: ${(imgCount - 2) * 400 * -1}`);
+    }
   });
 };
 
